f85ce3fbe72707e969fd5d9468ccaed1ac311fd5
oshanLahiru0307 <klahiruoshan@gmail.com>
2026-06-18 10:16:16 +0530

    user routes are updated

--- Routes/UserRoutes.py
+++ Routes/UserRoutes.py
@@ -11,13 +11,13 @@ router = APIRouter(
 )
 
 # get all users
-@router.get("/", response_model=list[User])
+@router.get("/", response_model=list[UserResponse])
 def get_users(db: Session = Depends(get_db)):
     users = db.query(User).all()
     return users
 
 # get user by id
-@router.get("/{user_id}", response_model=User)
+@router.get("/{user_id}", response_model=UserResponse)
 def get_user(user_id: int, db: Session = Depends(get_db)):
     user = db.query(User).filter(User.id == user_id).first()
     if not user:
@@ -25,7 +25,7 @@ def get_user(user_id: int, db: Session = Depends(get_db)):
     return user
 
 #update user by id
-@router.put("/{user_id}", response_model=User)
+@router.put("/{user_id}", response_model=UserResponse)
 def update_user(user_id: int, user: UserCreate, db: Session = Depends(get_db)):
     db_user = db.query(User).filter(User.id == user_id).first()
     if not db_user:
